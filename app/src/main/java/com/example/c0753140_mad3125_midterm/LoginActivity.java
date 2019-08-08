d53776f6a3d30360c4d30d5ad7bca58f6954cf4d
singhg1696 <50931443+singhg1696@users.noreply.github.com>
2019-08-08 19:22:51 -0400

    [Gurwinder Singh] Login Using Default Id & Password of Firebase

--- app/src/main/java/com/example/c0753140_mad3125_midterm/LoginActivity.java
+++ app/src/main/java/com/example/c0753140_mad3125_midterm/LoginActivity.java
@@ -1,25 +1,71 @@
 package com.example.c0753140_mad3125_midterm;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
     private EditText editTextEmail,editTextPass;
-    Button btnLogin;
+    private Button btnLogin;
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         getIds();
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                btnLogin.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        mAuth.signInWithEmailAndPassword(editTextEmail.getText().toString(), editTextPass.getText().toString())
+                                .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<AuthResult> task) {
+                                        if (task.isSuccessful()) {
+                                            // Sign in success, update UI with the signed-in user's information
+                                            
+                                        } else {
+                                            // If sign in fails, display a message to the user.
+                                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                                    Toast.LENGTH_SHORT).show();
+                                        }
+
+                                        // ...
+                                    }
+                                });
+                    }
+                });
+            }
+        });
     }
 
+    // function to get each id from xml file
     private void getIds(){
         editTextEmail = findViewById(R.id.txtEmail);
         editTextPass = findViewById(R.id.txtPass);
         btnLogin = findViewById(R.id.btnLogin);
     }
+
+
+
+
+
+
 }
